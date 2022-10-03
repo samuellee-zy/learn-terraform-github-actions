3712c4efd6c5cfd809da1d506b1435f612bef657
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2022-10-04 09:50:04 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "samuelleezy-hashicorp-demo"
+    organization = "samuellee-org"
 
     workspaces {
       name = "gh-actions-demo"
